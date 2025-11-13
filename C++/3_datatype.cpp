acd198923b9e4b5e0c3ae876d336c22da23af8cc
Yash-Tibadia <tibadiyayash@gmail.com>
2025-11-13 23:53:12 +0530

    data type

---
+++ C++/3_datatype.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  // int & long
+  int x = 10;
+  long y = 10101010;
+
+  // float & double
+  float a = 2.5;
+  double b = 2.33334;
+
+  // String
+  string name = "Yash";
+  cout << "Ans : " << name << endl; // Yash
+
+  string full_name;
+  getline(cin, full_name);
+  cout << "Ans : " << full_name << endl;
+
+  // Char
+  char ch = 'a';
+  cout << "Ans : " << ch;
+
+  return 0;
+}
