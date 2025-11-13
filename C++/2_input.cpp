2a48153296d1bd30dd762370282c3f8a045111bb
Yash-Tibadia <tibadiyayash@gmail.com>
2025-11-13 23:23:52 +0530

    inputs

---
+++ C++/2_input.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int x;
+  cin >> x;
+  cout << "value of x: " << x << endl;
+
+  int a,b;
+  cin >> a >> b;
+  cout << "Value of a: " << a << " and b: " << b;
+
+  return 0;
+}
